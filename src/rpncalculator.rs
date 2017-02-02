4316c64158f48b895c299194e338828e6cdc1c81
André Roque Matheus <amatheus@mac.com>
2017-02-02 18:35:18 -0200

    Error results for calculator evaluation

--- src/rpncalculator.rs
+++ src/rpncalculator.rs
@@ -1,22 +1,44 @@
+use std::num;
+
 pub struct RpnCalculator {
     stack: Vec<f64>,
 }
+
+#[derive(Debug)]
+enum RpnCalculatorError {
+    EmptyStack,
+    ParsingError,
+}
+
+impl From<num::ParseFloatError> for RpnCalculatorError {
+    fn from(_: num::ParseFloatError) -> RpnCalculatorError {
+        RpnCalculatorError::ParsingError
+    }
+}
+
+type Result = std::result::Result<(), RpnCalculatorError>;
 
 impl RpnCalculator {
     fn new() -> RpnCalculator {
         RpnCalculator { stack: Vec::new() }
     }
 
-    fn evaluate(&mut self, input: &str) {
-        let tokens = input.split_whitespace();
-        for token in tokens {
-            match token {
-                "+" => self.add_two(),
-                _ => {
-                    let value: f64 = token.parse().unwrap();
-                    self.stack.push(value);
-                }
+    fn evaluate(&mut self, input: &str) -> Result {
+        let mut tokens = input.split_whitespace();
+        loop {
+            let next = tokens.next();
+            match next {
+                None => break,
+                Some(token) => self.parse_token(token)?,
             }
+        }
+        Ok(())
+    }
+
+    fn parse_token(&mut self, token: &str) -> Result {
+        match token {
+            "+" => self.add_two(),
+            _ => self.parse_and_push(token),
         }
     }
 
@@ -28,11 +50,18 @@ impl RpnCalculator {
         self.stack.pop().unwrap()
     }
 
-    fn add_two(&mut self) {
-        let x = self.stack.pop().unwrap();
-        let y = self.stack.pop().unwrap();
+    fn parse_and_push(&mut self, token: &str) -> Result {
+        let value: f64 = token.parse()?;
+        self.stack.push(value);
+        Ok(())
+    }
+
+    fn add_two(&mut self) -> Result {
+        let x = self.stack.pop().ok_or(RpnCalculatorError::EmptyStack)?;
+        let y = self.stack.pop().ok_or(RpnCalculatorError::EmptyStack)?;
         let s = x + y;
         self.stack.push(s);
+        Ok(())
     }
 }
 
@@ -52,14 +81,21 @@ mod tests {
     #[test]
     fn should_add_f64_to_stack() {
         let mut calc = make_calculator();
-        calc.evaluate("2.5");
+        calc.evaluate("2.5").unwrap();
         assert_eq!(2.5, calc.top());
+    }
+
+    #[test]
+    fn should_return_error_when_evaluating_garbage() {
+        let mut calc = make_calculator();
+        let result = calc.evaluate("garbage");
+        assert!(result.is_err());
     }
 
     #[test]
     fn should_add_two_f64_to_stack() {
         let mut calc = make_calculator();
-        calc.evaluate("2.5 3.2");
+        calc.evaluate("2.5 3.2").unwrap();
         assert_eq!(3.2, calc.top());
         calc.pop();
         assert_eq!(2.5, calc.top());
@@ -68,7 +104,14 @@ mod tests {
     #[test]
     fn should_add_two_f64_in_stack() {
         let mut calc = make_calculator();
-        calc.evaluate("2.5 3.2 +");
+        calc.evaluate("2.5 3.2 +").unwrap();
         assert_eq!(5.7, calc.top());
+    }
+
+    #[test]
+    fn should_return_error_when_adding_with_empty_stack() {
+        let mut calc = make_calculator();
+        let result = calc.evaluate("+");
+        assert!(result.is_err());
     }
 }
