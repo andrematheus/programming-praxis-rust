50d1eb388a8c7b3cd4b1bbb8e3dcd6c4dd9c525d
André Roque Matheus <amatheus@mac.com>
2017-02-02 17:05:24 -0200

    Add two numbers to the stack

--- src/rpncalculator.rs
+++ src/rpncalculator.rs
@@ -8,12 +8,19 @@ impl RpnCalculator {
     }
 
     fn evaluate(&mut self, input: &str) {
-        let value: f64 = input.parse().unwrap();
-        self.stack.push(value);
+        let tokens = input.split_whitespace();
+        for token in tokens {
+            let value: f64 = token.parse().unwrap();
+            self.stack.push(value);
+        }
     }
 
     fn top(&self) -> f64 {
         *self.stack.last().unwrap()
+    }
+
+    fn pop(&mut self) -> f64{
+        self.stack.pop().unwrap()
     }
 }
 
@@ -34,6 +41,15 @@ mod tests {
     fn should_add_f64_to_stack() {
         let mut calc = make_calculator();
         calc.evaluate("2.5");
+        assert_eq!(2.5, calc.top());
+    }
+
+    #[test]
+    fn should_add_two_f64_to_stack() {
+        let mut calc = make_calculator();
+        calc.evaluate("2.5 3.2");
+        assert_eq!(3.2, calc.top());
+        calc.pop();
         assert_eq!(2.5, calc.top());
     }
 }
