4b3b2418ffbb49f78c20ce0b8adce1ddde4fdcd0
André Roque Matheus <amatheus@mac.com>
2017-02-03 11:07:09 -0200

    Customizable operators

--- src/rpncalculator.rs
+++ src/rpncalculator.rs
@@ -1,11 +1,13 @@
 use std::num;
+use std::collections;
 
 pub struct RpnCalculator {
-    stack: Vec<f64>,
+    stack: CalcStack,
+    operators: OperatorsMap,
 }
 
 #[derive(Debug)]
-enum RpnCalculatorError {
+pub enum RpnCalculatorError {
     EmptyStack,
     ParsingError,
 }
@@ -16,11 +18,28 @@ impl From<num::ParseFloatError> for RpnCalculatorError {
     }
 }
 
-type Result = std::result::Result<(), RpnCalculatorError>;
+pub type Result = std::result::Result<(), RpnCalculatorError>;
+pub type CalcStack = Vec<f64>;
+pub type OperatorFn = fn(&mut CalcStack) -> Result;
+pub type OperatorsMap = collections::BTreeMap<&'static str, OperatorFn>;
+
+fn add_two(s: &mut CalcStack) -> Result {
+    let x = s.pop().ok_or(RpnCalculatorError::EmptyStack)?;
+    let y = s.pop().ok_or(RpnCalculatorError::EmptyStack)?;
+    let result = x + y;
+    s.push(result);
+    Ok(())
+}
 
 impl RpnCalculator {
     fn new() -> RpnCalculator {
-        RpnCalculator { stack: Vec::new() }
+        let mut default_operators: OperatorsMap = collections::BTreeMap::new();
+        default_operators.insert("+", add_two);
+        RpnCalculator { stack: Vec::new(), operators: default_operators }
+    }
+
+    fn new_with_operators(operators: OperatorsMap) -> RpnCalculator {
+        RpnCalculator { stack: Vec::new(), operators: operators }
     }
 
     fn evaluate(&mut self, input: &str) -> Result {
@@ -36,9 +55,11 @@ impl RpnCalculator {
     }
 
     fn parse_token(&mut self, token: &str) -> Result {
-        match token {
-            "+" => self.add_two(),
-            _ => self.parse_and_push(token),
+        if self.operators.contains_key(token) {
+            let operator = self.operators.get(token).expect("Already checked if operators contains token");
+            operator(&mut self.stack)
+        } else {
+            self.parse_and_push(token)
         }
     }
 
@@ -55,22 +76,19 @@ impl RpnCalculator {
         self.stack.push(value);
         Ok(())
     }
-
-    fn add_two(&mut self) -> Result {
-        let x = self.stack.pop().ok_or(RpnCalculatorError::EmptyStack)?;
-        let y = self.stack.pop().ok_or(RpnCalculatorError::EmptyStack)?;
-        let s = x + y;
-        self.stack.push(s);
-        Ok(())
-    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections;
 
     fn make_calculator() -> RpnCalculator {
         RpnCalculator::new()
+    }
+
+    fn make_calculator_with_operators(operators: OperatorsMap) -> RpnCalculator {
+        RpnCalculator::new_with_operators(operators)
     }
 
     #[test]
@@ -105,13 +123,30 @@ mod tests {
     fn should_add_two_f64_in_stack() {
         let mut calc = make_calculator();
         calc.evaluate("2.5 3.2 +").unwrap();
-        assert_eq!(5.7, calc.top());
+        assert_eq!(5.7, calc.top(), "Calcultor's top should be result of addition");
     }
 
     #[test]
     fn should_return_error_when_adding_with_empty_stack() {
         let mut calc = make_calculator();
         let result = calc.evaluate("+");
-        assert!(result.is_err());
+        assert!(result.is_err(), "Should return error because '+' expects two operands");
     }
+
+    #[test]
+    fn should_use_operator_passed_at_construction_time() {
+        let mut operators: OperatorsMap = collections::BTreeMap::new();
+        fn test_op(s: &mut CalcStack) -> Result {
+            s.push(10.0);
+            Ok(())
+        }
+        operators.insert("?", test_op);
+        let mut calc = make_calculator_with_operators(operators);
+        let result = calc.evaluate("?");
+        assert!(result.is_ok(), "Should return ok as input is valid");
+        assert_eq!(10.0, calc.top(), "Should have returned value at the top");
+    }
+}
+
+fn main() {
 }
